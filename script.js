c13373c7bebd7904549528b800d6bb954f1e621d
Bibesh Basnet <bbibesh8@gmail.com>
2022-12-13 13:49:38 +0545

    about changes

--- script.js
+++ script.js
@@ -17,18 +17,22 @@ function showHobbies(){
   skills.style.display = "none";
   hobbies.style.display = "block";
 }
-
+// here we select all links by using querySelectorAll 
 const links = document.querySelectorAll('a ul li')
-
+// then we make  loop to add eventlistener => click
+// when the  thing is clicked it will fair the  function clickHandler
 for(const link of links){
   link.addEventListener('click',clickHandler())
 }
-
+// here we defien the function
 function clickHandler(e) {
   e.preventDefault();
+  //on the top of the section
   const href = this.getAttribute("href");
   const offsetTop = document.querySelector(href).offsetTop;
-
+// here we do the scroll
+// i don't know if that clear or not? yesssss hey 
+// 
   scroll({
     top: offsetTop,
     behavior: "smooth"
